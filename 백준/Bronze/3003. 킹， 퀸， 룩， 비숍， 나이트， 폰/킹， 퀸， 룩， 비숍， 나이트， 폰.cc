cb408796d46d94dd201a28b1d0deb1402f692240
Soo-David-Lee <slee1060@ucr.edu>
2024-08-14 14:46:17 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+   int a,b,c,d,e,f;
+   cin >> a >> b >> c >> d >> e >>f;
+
+   cout << (1 - a) << " " << (1 - b) << " " << (2 - c) << " " ;
+   cout << (2 - d) << " " << (2 - e) << " " << (8 - f) << " " << endl;
+}
