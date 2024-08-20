8eec79cedc6733f53c5e83ee096809a2274f934f
Soo-David-Lee <slee1060@ucr.edu>
2024-08-20 15:07:31 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+   int a, b;
+   cin >> a >> b;
+
+   cout << a + b << endl << a - b << endl << a * b << endl << a / b << endl << a % b << endl;
+}
