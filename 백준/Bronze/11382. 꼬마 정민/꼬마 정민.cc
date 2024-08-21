d40264d79da43b7d4777e8324871d151c880b26d
Soo-David-Lee <slee1060@ucr.edu>
2024-08-21 14:42:00 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+   long long int a, b, c;
+   cin >> a >> b >> c;
+
+   cout << a + b + c << endl;
+}
