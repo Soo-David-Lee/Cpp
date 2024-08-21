09562bad6d400a94fe1f3e5eb6ec5c0bad53fcb8
Soo-David-Lee <slee1060@ucr.edu>
2024-08-21 14:24:57 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+   int a, b;
+   cin >> a >> b;
+
+   cout << a * (b % 10) << endl;
+   cout << a * ((b / 10) % 10) << endl;
+   cout << a * (b / 100) << endl;
+   cout << (a * b) << endl;
+}
